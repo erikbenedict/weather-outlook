5c6f328cde1e04433594da6f3dd6cb998a1fc0a6
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-04-19 08:11:09 -0400

    added event listener for keyup on 'Enter' to run saveSearch function

--- assets/script.js
+++ assets/script.js
@@ -7,18 +7,18 @@ function saveSearch(event) {
   event.preventDefault();
   // * Get the existing saved cities from local storage
   const savedCities = localStorage.getItem('recentCities');
-  // If there are no saved cities, create a new empty array
+  // * If there are no saved cities, create a new empty array
   let recentCities = [];
   if (savedCities) {
     recentCities = JSON.parse(savedCities);
   }
   // * Check if the current city is already in the array
   if (!recentCities.includes(cityName.value)) {
-    // Add the current city to the array
+    // * Add the current city to the array
     recentCities.push(cityName.value);
-    // stringify array and store it in local storage
+    // * stringify array and store it in local storage
     localStorage.setItem('recentCities', JSON.stringify(recentCities));
-    // Create a button for the current city and add it to the recentSearch element
+    // * Create a button for the current city and add it to the recentSearch element
     const recentCity = document.createElement('button');
     recentCity.textContent = cityName.value;
     recentCity.classList.add('btn', 'btn-secondary', 'd-block', 'w-100', 'my-3');
@@ -53,7 +53,13 @@ function clearSearchHistory() {
     // * Clear the "recentCities" key from local storage
     localStorage.removeItem('recentCities');
   }
-  
-displayRecentSearches();
+
+//  * Event listeners and called functions
+cityName.addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    saveSearch(event);
+  }
+});
 searchBtn.addEventListener('click', saveSearch);
 clearBtn.addEventListener('click', clearSearchHistory);
+displayRecentSearches();
