96b0791562c92aa446b3bbed72e2ba4e2cf17f4d
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-04-23 20:36:39 -0400

    updated TODO

--- assets/script.js
+++ assets/script.js
@@ -89,6 +89,7 @@ const currentWeather = (lat, lon) => {
 }
 
 // * Calling all required data for 5 day forecast and displaying in forecastContainer
+// TODO: In bug/forecast branch, use One Call API 16 day forecast and set cnt=5 to get 5 day forecast once authorized
 const forecast = (lat, lon) => {
   fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`)
   .then(response => response.json())
